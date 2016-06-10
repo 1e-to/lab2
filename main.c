2ebeaafc4b07d4c65f512a59063ee9cbf955fdd2
totoro4 <totmeninal@mail.ru>
2016-06-10 17:46:01 +0600

    Added assigment from 'argv'

--- main.c
+++ main.c
@@ -8,5 +8,11 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "Please, enter your coefficients!\n");
 		exit(EXIT_FAILURE);
 	}
+
+	int a, b, c;
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	c = atoi(argv[3]);
+
 	return EXIT_SUCCESS;
 }
